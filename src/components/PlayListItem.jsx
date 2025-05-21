82178fddac6f8a45ee740ab333158ae2d6981065
“kelciatkinson” <kelciatkinson@gmail.com>
2025-05-21 11:05:18 -0500

    hover transition on buttons and playlist item

--- src/components/PlayListItem.jsx
+++ src/components/PlayListItem.jsx
@@ -1,46 +1,11 @@
 export default function PlayListItem({title, artist, length}) {
   return <div className="space-y-1">
-    <div className="flex justify-between bg-(--color-accent) dark:bg-(--color-dark-accent) px-0.25 pb-0.5 rounded-lg">
+    <div className="group flex justify-between hover:bg-(--color-accent) transition duration-300 ease-in-out dark:bg-(--color-dark-accent) px-0.25 pb-0.5 rounded-lg">
       <div>
-        <p className="font-medium text-(--color-text) dark:text-(--color-text)">Painted in Blue</p>
-        <p className="font-medium text-sm text-(--color-text) dark:text-(--color-text) pb-1">Soul Canvas</p>
+        <p className="font-medium text-(--color-text) group-hover:text-(--color-text) transition duration-300 ease-in-out dark:text-(--color-text)">Painted in Blue</p>
+        <p className="font-medium text-sm text-(--color-secondary) group-hover:text-(--color-text) transition duration-300 ease-in-out dark:text-(--color-dark-secondary) pb-1">Soul Canvas</p>
       </div>
-      <p className="self-center font-medium text-sm text-(--color-text) dark:text-(--color-text)">5:55</p>
-    </div>
-    <div className="flex justify-between px-0.25 pb-0.5">
-      <div className="">
-        <p className="font-medium text-(--color-text) dark:text-(--color-dark-text)">Title Drift</p>
-        <p className="font-medium text-sm text-(--color-secondary) dark:text-(--color-dark-secondary) pb-1">Echoes of the Sea</p>
-      </div>
-      <p className="self-center font-medium text-sm text-(--color-secondary) dark:text-(--color-dark-secondary)">8:02</p>
-    </div>
-    <div className="flex justify-between px-0.25 pb-0.5">
-      <div className="">
-        <p className="font-medium text-(--color-text) dark:text-(--color-dark-text)">Fading Shadows</p>
-        <p className="font-medium text-sm text-(--color-secondary) dark:text-(--color-dark-secondary) pb-1">The Emberlight</p>
-      </div>
-      <p className="self-center font-medium text-sm text-(--color-secondary) dark:text-(--color-dark-secondary)">3:01</p>
-    </div>
-    <div className="flex justify-between px-0.25 pb-0.5">
-      <div className="">
-        <p className="font-medium text-(--color-text) dark:text-(--color-dark-text)">Cosmic Drift</p>
-        <p className="font-medium text-sm text-(--color-secondary) dark:text-(--color-dark-secondary) pb-1">Solar Flare</p>
-      </div>
-      <p className="self-center font-medium text-sm text-(--color-secondary) dark:text-(--color-dark-secondary)">5:01</p>
-    </div>
-    <div className="flex justify-between px-0.25 pb-0.5">
-      <div className="">
-        <p className="font-medium text-(--color-text) dark:text-(--color-dark-text)">Urban Serenade</p>
-        <p className="font-medium text-sm text-(--color-secondary) dark:text-(--color-dark-secondary) pb-1">Midnight Groove</p>
-      </div>
-      <p className="self-center font-medium text-sm text-(--color-secondary) dark:text-(--color-dark-secondary)">4:54</p>
-    </div>
-    <div className="flex justify-between px-0.25 pb-0.5">
-      <div className="">
-        <p className="font-medium text-(--color-text) dark:text-(--color-dark-text)">Whispers in the Wind</p>
-        <p className="font-medium text-sm text-(--color-secondary) dark:text-(--color-dark-secondary) pb-1">Rust & Rain</p>
-      </div>
-      <p className="self-center font-medium text-sm text-(--color-secondary) dark:text-(--color-dark-secondary)">6:13</p>
+      <p className="self-center font-medium text-sm text-(--color-secondary) group-hover:text-(--color-text) transition duration-300 ease-in-out dark:text-(--color-text)">5:55</p>
     </div>
   </div>
 }
--- src/components/VolumeControls.jsx
+++ src/components/VolumeControls.jsx
@@ -2,7 +2,7 @@ import volume from "../assets/volume-2.svg";
 
 export default function VolumeControls() {
   return <div className="w-100 h-4 flex items-center -mt-62">
-      <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 stroke-(--color-text) dark:stroke-(--color-dark-text)" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume2-icon lucide-volume-2"><path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z"/><path d="M16 9a5 5 0 0 1 0 6"/><path d="M19.364 18.364a9 9 0 0 0 0-12.728"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 stroke-[var(--color-text)] hover:stroke-(--color-dark-text) transition duration-300 ease-in-out dark:stroke-(--color-dark-text)" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-volume2-icon lucide-volume-2"><path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z"/><path d="M16 9a5 5 0 0 1 0 6"/><path d="M19.364 18.364a9 9 0 0 0 0-12.728"/></svg>
       <input type="range" defaultValue="50" className="w-full accent-(--color-primary) dark:accent-(--color-dark-primary)"></input>
   </div>
 }

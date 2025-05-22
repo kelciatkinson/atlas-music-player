c63fe1989149da38d141d985bd2294a5eaefea30
“kelciatkinson” <kelciatkinson@gmail.com>
2025-05-22 10:12:58 -0500

    add base url config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(),
-    tailwindcss(),
+  tailwindcss(),
   ],
+  base: './',
+  build: {
+    outDir: 'dist'
+  }
 });

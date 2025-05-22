ef9f1c1bad9c3ba079d519b5752fd067bb5257ad
“kelciatkinson” <kelciatkinson@gmail.com>
2025-05-22 09:29:11 -0500

    removed typescript files, pushing to have working app

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import CurrentlyPlaying from "./CurrentlyPlaying";
 import Playlist from "./Playlist";
 
@@ -8,3 +9,4 @@ export default function MusicPlayer() {
     <Playlist />
   </div>
 }
+ 
